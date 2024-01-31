a33394dca152ae5cd70129141dfa6bb5ad02e49c
Dmitry Moroz <dimasbka@gmail.com>
2024-01-31 20:24:15 +0300

    добавил тесты для апи причесал код

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase,  Client
+from geo.models import City
+
+class ApiTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_API_empty_city(self):
+        response = self.client.get( "/weather", )
+        assert response.status_code == 400
+
+    def test_API_city_not_exists(self):
+        response = self.client.get(
+                "/weather",
+                data={ 'city':'Арканар' },
+                headers={' Content-Type':'application/json' },
+            )
+        assert response.status_code == 404
+
+    def test_API_city_exists(self):
+        response = self.client.get(
+                "/weather",
+                data={ 'city':'Краснодар' },
+                headers={' Content-Type':'application/json' },
+            )
+        assert response.status_code == 200
